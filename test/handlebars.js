755f854c50cf13e95021dd92865a9256490e92c3
wycats <wycats@gmail.com>
2010-07-31 12:00:11 -0700

    Reorganize things and remove unneeded dependency on jQuery

---
+++ test/handlebars.js
@@ -0,0 +1,17 @@
+module("basic context");
+
+test("compiling with a basic context", function() {
+  var string   = "Goodbye\n{{cruel}}\n{{world}}";
+  var template = Handlebars.compile(string);
+
+  result = template({cruel: "cruel", world: "world"});
+  equal("Goodbye\ncruel\nworld", result, "it works if all the required keys are provided");
+});
+
+test("comments", function() {
+  var string   = "{{! Goodbye}}Goodbye\n{{cruel}}\n{{world}}";
+  var template = Handlebars.compile(string);
+
+  result = template({cruel: "cruel", world: "world"});
+  equal("Goodbye\ncruel\nworld", result, "it works if all the required keys are provided");
+});
