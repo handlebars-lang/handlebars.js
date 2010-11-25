4b2e543d2e18e3b1acc480615755dfa96eaca121
wycats <wycats@gmail.com>
2010-11-25 11:52:35 -0800

    Remove node dependency from handlebars.js

--- test.js
+++ test.js
@@ -5,4 +5,4 @@ var Handlebars = require("handlebars").Handlebars;
 var string = "foo {{ bar baz \"baz\" }}baz{{! foo bar baz }}{{#foo}} bar {{^}} baz {{/foo}}{{> partial }}{{# bar }}part1 {{^}} part2{{> foo }}{{/bar}}zomg"
 
 var ast = Handlebars.parse(string);
-Handlebars.print(ast);
+require("sys").print(Handlebars.print(ast));
