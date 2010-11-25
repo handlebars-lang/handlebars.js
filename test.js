d97bc591a2abab57646bda9a5e5f631958dc15d2
wycats <wycats@gmail.com>
2010-11-25 02:49:08 -0800

    Restructure files to make more sense in a non-CommonJS world.

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 require.paths.push(__dirname + "/lib");
 
-var Handlebars = require("handlebars");
+var Handlebars = require("handlebars").Handlebars;
 
 var string = "foo {{ bar baz \"baz\" }}baz{{! foo bar baz }}{{#foo}} bar {{^}} baz {{/foo}}{{> partial }}{{# bar }}part1 {{^}} part2{{> foo }}{{/bar}}zomg"
 
