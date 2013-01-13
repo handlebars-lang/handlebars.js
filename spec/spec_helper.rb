f671c2e3d7bd297ea505502a7478080f63e02145
kpdecker <kpdecker@gmail.com>
2013-01-13 12:43:42 -0600

    Pin rubyracer version to 0.10
    This is a temporary fix. See https://github.com/wycats/handlebars.js/pull/405#issuecomment-12013108

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,32 @@
 require "v8"
 
+# Monkey patches due to bugs in RubyRacer
+class V8::JSError
+  def initialize(try, to)
+    @to = to
+    begin
+      super(initialize_unsafe(try))
+    rescue Exception => e
+      # Original code does not make an Array here
+      @boundaries = [Boundary.new(:rbframes => e.backtrace)]
+      @value = e
+      super("BUG! please report. JSError#initialize failed!: #{e.message}")
+    end
+  end
+
+  def parse_js_frames(try)
+    raw = @to.rb(try.StackTrace())
+    if raw && !raw.empty?
+      raw.split("\n")[1..-1].tap do |frames|
+        # Original code uses strip!, and the frames are not guaranteed to be strippable
+        frames.each {|frame| frame.strip.chomp!(",")}
+      end
+    else
+      []
+    end
+  end
+end
+
 module Handlebars
   module Spec
     def self.js_backtrace(context)
