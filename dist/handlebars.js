bc8214cb7cb4b04180f4df33439ac61a92e1123f
Peter Wagenet <peter.wagenet@gmail.com>
2013-02-14 21:58:20 -0800

    Updated to 1.0.0-rc.3

--- dist/handlebars.js
+++ dist/handlebars.js
@@ -29,12 +29,12 @@ this.Handlebars = {};
 
 (function(Handlebars) {
 
-Handlebars.VERSION = "1.0.rc.2";
+Handlebars.VERSION = "1.0.0-rc.3";
 Handlebars.COMPILER_REVISION = 2;
 
 Handlebars.REVISION_CHANGES = {
   1: '<= 1.0.rc.2', // 1.0.rc.2 is actually rev2 but doesn't report it
-  2: '>= 1.0.rc.3'
+  2: '>= 1.0.0-rc.3'
 };
 
 Handlebars.helpers  = {};
