497515c02fd58d46e38c872a3fbf366be5f876fe
kpdecker <kpdecker@gmail.com>
2013-07-24 23:43:56 -0500

    Remove braces on single line lex statements
    Resolves a parsing issue in the 0.4 branch of jison.

--- dist/handlebars.js
+++ dist/handlebars.js
@@ -566,7 +566,7 @@ lexer.performAction = function anonymous(yy,yy_,$avoiding_name_collisions,YY_STA
 
 var YYSTATE=YY_START
 switch($avoiding_name_collisions) {
-case 0: yy_.yytext = "\\"; return 14; 
+case 0:yy_.yytext = "\\"; return 14;
 break;
 case 1:
                                    if(yy_.yytext.slice(-1) !== "\\") this.begin("mu");
@@ -574,7 +574,7 @@ case 1:
                                    if(yy_.yytext) return 14;
                                  
 break;
-case 2: return 14; 
+case 2:return 14;
 break;
 case 3:
                                    if(yy_.yytext.slice(-1) !== "\\") this.popState();
@@ -582,61 +582,61 @@ case 3:
                                    return 14;
                                  
 break;
-case 4: yy_.yytext = yy_.yytext.substr(0, yy_.yyleng-4); this.popState(); return 15; 
+case 4:yy_.yytext = yy_.yytext.substr(0, yy_.yyleng-4); this.popState(); return 15;
 break;
-case 5: return 25; 
+case 5:return 25;
 break;
-case 6: return 16; 
+case 6:return 16;
 break;
-case 7: return 20; 
+case 7:return 20;
 break;
-case 8: return 19; 
+case 8:return 19;
 break;
-case 9: return 19; 
+case 9:return 19;
 break;
-case 10: return 23; 
+case 10:return 23;
 break;
-case 11: return 22; 
+case 11:return 22;
 break;
-case 12: this.popState(); this.begin('com'); 
+case 12:this.popState(); this.begin('com');
 break;
-case 13: yy_.yytext = yy_.yytext.substr(3,yy_.yyleng-5); this.popState(); return 15; 
+case 13:yy_.yytext = yy_.yytext.substr(3,yy_.yyleng-5); this.popState(); return 15;
 break;
-case 14: return 22; 
+case 14:return 22;
 break;
-case 15: return 39; 
+case 15:return 39;
 break;
-case 16: return 38; 
+case 16:return 38;
 break;
-case 17: return 38; 
+case 17:return 38;
 break;
-case 18: return 42; 
+case 18:return 42;
 break;
-case 19: /*ignore whitespace*/ 
+case 19:/*ignore whitespace*/
 break;
-case 20: this.popState(); return 24; 
+case 20:this.popState(); return 24;
 break;
-case 21: this.popState(); return 18; 
+case 21:this.popState(); return 18;
 break;
-case 22: yy_.yytext = yy_.yytext.substr(1,yy_.yyleng-2).replace(/\\"/g,'"'); return 32; 
+case 22:yy_.yytext = yy_.yytext.substr(1,yy_.yyleng-2).replace(/\\"/g,'"'); return 32;
 break;
-case 23: yy_.yytext = yy_.yytext.substr(1,yy_.yyleng-2).replace(/\\'/g,"'"); return 32; 
+case 23:yy_.yytext = yy_.yytext.substr(1,yy_.yyleng-2).replace(/\\'/g,"'"); return 32;
 break;
-case 24: return 40; 
+case 24:return 40;
 break;
-case 25: return 34; 
+case 25:return 34;
 break;
-case 26: return 34; 
+case 26:return 34;
 break;
-case 27: return 33; 
+case 27:return 33;
 break;
-case 28: return 38; 
+case 28:return 38;
 break;
-case 29: yy_.yytext = yy_.yytext.substr(1, yy_.yyleng-2); return 38; 
+case 29:yy_.yytext = yy_.yytext.substr(1, yy_.yyleng-2); return 38;
 break;
-case 30: return 'INVALID'; 
+case 30:return 'INVALID';
 break;
-case 31: return 5; 
+case 31:return 5;
 break;
 }
 };
