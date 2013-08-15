0b92db7b0ccd5c347cfe2f14571f998b84185a2e
kpdecker <kpdecker@gmail.com>
2013-08-15 10:30:15 -0500

    Rebuild

--- dist/handlebars.runtime.js
+++ dist/handlebars.runtime.js
@@ -160,7 +160,7 @@ Handlebars.registerHelper('if', function(conditional, options) {
   var type = toString.call(conditional);
   if(type === functionType) { conditional = conditional.call(this); }
 
-  if(!conditional || Handlebars.Utils.isEmpty(conditional)) {
+  if(Handlebars.Utils.isEmpty(conditional)) {
     return options.inverse(this);
   } else {
     return options.fn(this);
@@ -202,7 +202,7 @@ Handlebars.SafeString = function(string) {
   this.string = string;
 };
 Handlebars.SafeString.prototype.toString = function() {
-  return this.string.toString();
+  return "" + this.string;
 };
 
 var escape = {
@@ -241,7 +241,7 @@ Handlebars.Utils = {
     // Force a string conversion as this will be done by the append regardless and
     // the regex test will do this transparently behind the scenes, causing issues if
     // an object's to string has escaped characters in it.
-    string = string.toString();
+    string = "" + string;
 
     if(!possible.test(string)) { return string; }
     return string.replace(badChars, escapeChar);
