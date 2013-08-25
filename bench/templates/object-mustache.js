57a0ee3d596f73db0a854f3a042ee94e3bae8bf0
kpdecker <kpdecker@gmail.com>
2013-08-25 13:46:45 -0500

    Remove duplicated tests

--- bench/templates/object-mustache.js
+++ bench/templates/object-mustache.js
@@ -1,6 +1,4 @@
 module.exports = {
   context: { person: { name: "Larry", age: 45 } },
-  handlebars: "{{#person}}{{name}}{{age}}{{/person}}",
-  dust: "{#person}{name}{age}{/person}",
-  mustache: "{{#person}}{{name}}{{age}}{{/person}}"
+  handlebars: "{{#person}}{{name}}{{age}}{{/person}}"
 };
