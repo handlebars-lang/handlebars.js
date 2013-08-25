709d57b3c15aa0d766152362b031ee9d27c054f4
kpdecker <kpdecker@gmail.com>
2013-08-25 15:28:52 -0500

    Add eco object test

--- bench/templates/object.js
+++ bench/templates/object.js
@@ -2,5 +2,6 @@ module.exports = {
   context: { person: { name: "Larry", age: 45 } },
   handlebars: "{{#with person}}{{name}}{{age}}{{/with}}",
   dust: "{#person}{name}{age}{/person}",
+  eco: "<%= @person.name %><%= @person.age %>",
   mustache: "{{#person}}{{name}}{{age}}{{/person}}"
 };
