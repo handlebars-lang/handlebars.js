f650b0d69ec70f82fc58022a530b95b0e4371f1d
kpdecker <kpdecker@gmail.com>
2014-01-15 09:41:53 -0600

    Expose the initial context via @root

--- spec/expected/empty.amd.js
+++ spec/expected/empty.amd.js
@@ -2,7 +2,7 @@ define(['handlebars.runtime'], function(Handlebars) {
   Handlebars = Handlebars["default"];  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 return templates['empty'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+helpers = this.merge(helpers, Handlebars.helpers); data = this.initData(depth0, data);
   var buffer = "";
 
 
