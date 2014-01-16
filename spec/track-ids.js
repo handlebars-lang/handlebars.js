9df919083d4e25512fb6dc6fe1a050e63ad12c80
kpdecker <kpdecker@gmail.com>
2014-01-16 13:59:02 -0600

    Create track ids test stub

---
+++ spec/track-ids.js
@@ -0,0 +1,6 @@
+describe('track ids', function() {
+  it('should include argument ids');
+  it('should include hash ids');
+  it('should note ../ references');
+  it('should update the path when using built helpers');
+});
