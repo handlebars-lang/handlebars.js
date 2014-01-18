a14daca1c6bff532146cd4bae66fe12c94c44117
kpdecker <kpdecker@gmail.com>
2014-01-17 23:11:13 -0600

    Simplify sauce test environments

--- Gruntfile.js
+++ Gruntfile.js
@@ -141,14 +141,11 @@ module.exports = function(grunt) {
             {browserName: 'firefox', version: '3.6'},
             {browserName: 'safari', version: 7, platform: 'OS X 10.9'},
             {browserName: 'safari', version: 6, platform: 'OS X 10.8'},
-            {browserName: 'safari', version: 5},
             {browserName: 'opera', version: 12},
             {browserName: 'opera', version: 11},
             {browserName: 'internet explorer', version: 11, platform: 'Windows 8.1'},
             {browserName: 'internet explorer', version: 10, platform: 'Windows 8'},
             {browserName: 'internet explorer', version: 9, platform: 'Windows 7'},
-            {browserName: 'internet explorer', version: 8, platform: 'XP'},
-            {browserName: 'internet explorer', version: 7, platform: 'XP'},
             {browserName: 'internet explorer', version: 6, platform: 'XP'}
           ]
         }
