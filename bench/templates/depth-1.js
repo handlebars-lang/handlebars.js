6ca06d41156a43d29790d101d6aa97ed922ca8ad
kpdecker <kpdecker@gmail.com>
2014-02-06 22:50:12 -0800

    Add depth benchmark cases

---
+++ bench/templates/depth-1.js
@@ -0,0 +1,6 @@
+module.exports = {
+  context: { names: [{name: "Moe"}, {name: "Larry"}, {name: "Curly"}, {name: "Shemp"}], foo: 'bar' },
+  handlebars: "{{#each names}}{{../foo}}{{/each}}",
+  mustache: "{{#names}}{{foo}}{{/names}}",
+  eco: "<% for item in @names: %><%= @foo %><% end %>"
+};
---
+++ bench/templates/depth-2.js
@@ -0,0 +1,6 @@
+module.exports = {
+  context: { names: [{bat: 'foo', name: ["Moe"]}, {bat: 'foo', name: ["Larry"]}, {bat: 'foo', name: ["Curly"]}, {bat: 'foo', name: ["Shemp"]}], foo: 'bar' },
+  handlebars: "{{#each names}}{{#each name}}{{../bat}}{{../../foo}}{{/each}}{{/each}}",
+  mustache: "{{#names}}{{#name}}{{bat}}{{foo}}{{/name}}{{/names}}",
+  eco: "<% for item in @names: %><% for child in item.name: %><%= item.bat %><%= @foo %><% end %><% end %>"
+};
