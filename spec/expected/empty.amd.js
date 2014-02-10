6da3c314943d66aeec325b1c1d263ff4ace6406d
kpdecker <kpdecker@gmail.com>
2014-02-10 01:48:12 -0600

    Update compiler revision info

--- spec/expected/empty.amd.js
+++ spec/expected/empty.amd.js
@@ -1,6 +1,6 @@
 define(['handlebars.runtime'], function(Handlebars) {
   Handlebars = Handlebars["default"];  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-return templates['empty'] = template({"compiler":[4,">= 1.0.0"],"main":function(depth0,helpers,partials,data) {
+return templates['empty'] = template({"compiler":[5,">= 2.0.0"],"main":function(depth0,helpers,partials,data) {
   return "";
 },"useData":true});
 });
