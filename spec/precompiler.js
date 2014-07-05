059532295d022c775f6c3e6636a9d6ffce8da728
kpdecker <kpdecker@gmail.com>
2014-07-05 14:17:11 -0500

    Move precompiler to safer location
    Attempts to avoid some of the Node vs. ES6 confusion between the different styles within the application.
    
    Also adds some simple tests in addition to the integration test.

---
+++ spec/precompiler.js
@@ -0,0 +1,40 @@
+/*global shouldThrow */
+
+// NOP Under non-node environments
+if (typeof process === 'undefined') {
+  return;
+}
+
+var Handlebars = require('../lib'),
+    Precompiler = require('../lib/precompiler');
+
+describe('precompiler', function() {
+  var log,
+      logFunction;
+
+  beforeEach(function() {
+    logFunction = console.log;
+    log = '';
+    console.log = function() {
+      log += Array.prototype.join.call(arguments, '');
+    };
+  });
+  afterEach(function() {
+    console.log = logFunction;
+  });
+
+  it('should output version', function() {
+    Precompiler.cli({templates: [], version: true});
+    equals(log, Handlebars.VERSION);
+  });
+  it('should throw if lacking templates', function() {
+    shouldThrow(function() {
+      Precompiler.cli({templates: []});
+    }, Handlebars.Exception, 'Must define at least one template or directory.');
+  });
+  it('should throw on missing template', function() {
+    shouldThrow(function() {
+      Precompiler.cli({templates: ['foo']});
+    }, Handlebars.Exception, 'Unable to open template file "foo"');
+  });
+});
