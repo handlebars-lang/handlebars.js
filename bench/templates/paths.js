704961b37840a439d6576a0d8f0f595838ab0963
kpdecker <kpdecker@gmail.com>
2014-07-06 11:37:24 -0500

    Add additional paths to path throughput test

--- bench/templates/paths.js
+++ bench/templates/paths.js
@@ -1,7 +1,7 @@
 module.exports = {
-  context: { person: { name: "Larry", age: 45 } },
-  handlebars: "{{person.name}}{{person.age}}{{person.foo}}{{animal.age}}",
-  dust: "{person.name}{person.age}{person.foo}{animal.age}",
-  eco: "<%= @person.name %><%= @person.age %><%= @person.foo %><% if @animal: %><%= @animal.age %><% end %>",
-  mustache: "{{person.name}}{{person.age}}{{person.foo}}{{animal.age}}"
+  context: { person: { name: {bar: {baz: "Larry"}}, age: 45 } },
+  handlebars: "{{person.name.bar.baz}}{{person.age}}{{person.foo}}{{animal.age}}",
+  dust: "{person.name.bar.baz}{person.age}{person.foo}{animal.age}",
+  eco: "<%= @person.name.bar.baz %><%= @person.age %><%= @person.foo %><% if @animal: %><%= @animal.age %><% end %>",
+  mustache: "{{person.name.bar.baz}}{{person.age}}{{person.foo}}{{animal.age}}"
 };
