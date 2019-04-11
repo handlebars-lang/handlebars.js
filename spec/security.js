cd38583216dce3252831916323202749431c773e
Nils Knappmeier <github@knappi.org>
2019-04-11 17:46:27 +0200

    fix: prevent RCE through the "lookup"-helper
    - The "lookup" helper could also be used to run a remote code execution
      by manipulating the template. The same check as for regular
      path queries now also is done in the "lookup"-helper

--- spec/security.js
+++ spec/security.js
@@ -2,10 +2,14 @@ describe('security issues', function() {
     describe('GH-1495: Prevent Remote Code Execution via constructor', function() {
         it('should not allow constructors to be accessed', function() {
             shouldCompileTo('{{constructor.name}}', {}, '');
+            shouldCompileTo('{{lookup (lookup this "constructor") "name"}}', {}, '');
         });
 
         it('should allow the "constructor" property to be accessed if it is enumerable', function() {
             shouldCompileTo('{{constructor.name}}', {'constructor': {
+                'name': 'here we go'
+            }}, 'here we go');
+            shouldCompileTo('{{lookup (lookup this "constructor") "name"}}', {'constructor': {
                 'name': 'here we go'
             }}, 'here we go');
         });
@@ -23,6 +27,9 @@ describe('security issues', function() {
 
             shouldCompileTo('{{#with this as |obj|}}{{obj.abc}}{{/with}}',
                 new TestClass(), 'xyz');
+            shouldCompileTo('{{#with this as |obj|}}{{lookup obj "abc"}}{{/with}}',
+                new TestClass(), 'xyz');
+
         });
     });
 });
