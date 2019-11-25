4801f649df68617358582a766b9c76cdf0771901
Nils Knappmeier <github@knappi.org>
2019-11-25 23:01:36 +0100

    minor styling fixes in test

--- integration-testing/webpack-babel-test/src/handlebars-inline-precompile-test.js
+++ integration-testing/webpack-babel-test/src/handlebars-inline-precompile-test.js
@@ -1,12 +1,12 @@
-import * as Handlebars from 'handlebars/runtime'
+import * as Handlebars from 'handlebars/runtime';
 import hbs from 'handlebars-inline-precompile';
-import {assertEquals} from "../../webpack-test/src/lib/assert";
+import {assertEquals} from '../../webpack-test/src/lib/assert';
 
 Handlebars.registerHelper('loud', function(text) {
     return text.toUpperCase();
 });
 
 const template = hbs`{{loud name}}`;
-const output = template({name: 'yehuda'})
+const output = template({name: 'yehuda'});
 
-assertEquals(output, 'YEHUDA')
+assertEquals(output, 'YEHUDA');
