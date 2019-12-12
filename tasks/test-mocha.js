12dba0eab0de506403d15c9430cf24da0752b410
Nils Knappmeier <github@knappi.org>
2019-12-12 22:19:30 +0100

    test/style: refactor parser task

--- tasks/test-mocha.js
+++ tasks/test-mocha.js
@@ -1,66 +1,36 @@
-const childProcess = require('child_process');
+const { execNodeJsScriptWithInheritedOutput } = require('./util/exec-file');
+const { createRegisterAsyncTaskFn } = require('./util/async-grunt-task');
 
 module.exports = function(grunt) {
-  grunt.registerTask(
-    'test:mocha',
-    promiseBasedTask(async () => forkAndWait('./spec/env/runner'))
+  const registerAsyncTask = createRegisterAsyncTaskFn(grunt);
+
+  registerAsyncTask('test:mocha', async () =>
+    execNodeJsScriptWithInheritedOutput('./spec/env/runner')
   );
 
-  grunt.registerTask(
-    'test:cov',
-    promiseBasedTask(async () =>
-      forkAndWait(
-        'node_modules/istanbul/lib/cli.js',
-        'cover',
-        '--source-map',
-        '--',
-        './spec/env/runner.js'
-      )
-    )
+  registerAsyncTask('test:cov', async () =>
+    execNodeJsScriptWithInheritedOutput('node_modules/istanbul/lib/cli.js', [
+      'cover',
+      '--source-map',
+      '--',
+      './spec/env/runner.js'
+    ])
   );
 
-  grunt.registerTask(
-    'test:min',
-    promiseBasedTask(async () => forkAndWait('./spec/env/runner', '--min'))
+  registerAsyncTask('test:min', async () =>
+    execNodeJsScriptWithInheritedOutput('./spec/env/runner', ['--min'])
   );
 
-  grunt.registerTask(
-    'test:check-cov',
-    promiseBasedTask(() =>
-      forkAndWait(
-        'node_modules/istanbul/lib/cli.js',
-        'check-coverage',
-        '--statements',
-        '100',
-        '--functions',
-        '100',
-        '--branches',
-        '100',
-        '--lines 100'
-      )
-    )
+  registerAsyncTask('test:check-cov', async () =>
+    execNodeJsScriptWithInheritedOutput('node_modules/istanbul/lib/cli.js', [
+      'check-coverage',
+      '--statements',
+      '100',
+      '--functions',
+      '100',
+      '--branches',
+      '100',
+      '--lines 100'
+    ])
   );
-
-  function promiseBasedTask(asyncFunction) {
-    return function() {
-      asyncFunction()
-        .catch(error => {
-          grunt.fatal(error);
-        })
-        .finally(this.async());
-    };
-  }
-
-  async function forkAndWait(command, ...args) {
-    return new Promise((resolve, reject) => {
-      const child = childProcess.fork(command, args, { stdio: 'inherit' });
-      child.on('close', code => {
-        if (code !== 0) {
-          reject(new Error(`Child process failed with exit-code ${code}`));
-        }
-      });
-    });
-  }
-
-  grunt.registerTask('test', ['test:bin', 'test:cov', 'test:check-cov']);
 };
