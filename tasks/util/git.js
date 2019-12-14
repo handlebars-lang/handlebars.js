1ec1737d24a1a37e39ca4c24b0fdb7efd877e628
Nils Knappmeier <github@knappi.org>
2019-12-14 01:49:55 +0100

    test/style: refactor remaining grunt tasks to use promises instead of callbacks

--- tasks/util/git.js
+++ tasks/util/git.js
@@ -1,115 +1,74 @@
 const childProcess = require('child_process');
 
 module.exports = {
-  debug: function(callback) {
-    childProcess.exec('git remote -v', {}, function(err, remotes) {
-      if (err) {
-        throw new Error('git.remote: ' + err.message);
+  async remotes() {
+    return git('remotes', '-v');
+  },
+  async branches() {
+    return git('branch', '-a');
+  },
+  async clean() {
+    const stdout = git('diff-index', '--name-only', 'HEAD', '--');
+    return stdout === '';
+  },
+  async commitInfo() {
+    const headSha = await this.headSha();
+    const masterSha = await this.masterSha();
+    return {
+      headSha,
+      masterSha,
+      tagName: await this.tagName(),
+      isMaster: headSha === masterSha
+    };
+  },
+  async headSha() {
+    const stdout = await git(' rev-parse', '--short', 'HEAD');
+    return stdout.trim();
+  },
+  async masterSha() {
+    try {
+      const stdout = await git('rev-parse', '--short', 'origin/master');
+      return stdout.trim();
+    } catch (error) {
+      if (/Needed a single revision/.test(error.message)) {
+        // Master was not checked out but in this case, so we know we are not master. We can ignore this
+        return '';
       }
-
-      childProcess.exec('git branch -a', {}, function(err, branches) {
-        if (err) {
-          throw new Error('git.branch: ' + err.message);
-        }
-
-        callback(remotes, branches);
-      });
-    });
-  },
-  clean: function(callback) {
-    childProcess.exec('git diff-index --name-only HEAD --', {}, function(
-      err,
-      stdout
-    ) {
-      callback(undefined, !err && !stdout);
-    });
+      throw error;
+    }
   },
 
-  commitInfo: function(callback) {
-    module.exports.head(function(err, headSha) {
-      module.exports.master(function(err, masterSha) {
-        module.exports.tagName(function(err, tagName) {
-          callback(undefined, {
-            head: headSha,
-            master: masterSha,
-            tagName: tagName,
-            isMaster: headSha === masterSha
-          });
-        });
-      });
-    });
+  async add(path) {
+    return git('add', '-f', path);
   },
-
-  head: function(callback) {
-    childProcess.exec('git rev-parse --short HEAD', {}, function(err, stdout) {
-      if (err) {
-        throw new Error('git.head: ' + err.message);
-      }
-
-      callback(undefined, stdout.trim());
-    });
+  async commit(message) {
+    return git('commit', '--message', message);
   },
-  master: function(callback) {
-    childProcess.exec('git rev-parse --short origin/master', {}, function(
-      err,
-      stdout
-    ) {
-      // This will error if master was not checked out but in this case we know we are not master
-      // so we can ignore.
-      if (err && !/Needed a single revision/.test(err.message)) {
-        throw new Error('git.master: ' + err.message);
-      }
-
-      callback(undefined, stdout.trim());
-    });
+  async tag(name) {
+    return git('tag', '-a', `--message=${name}`, name);
   },
+  async tagName() {
+    const stdout = await git('tag', '-l', '--points-at', 'HEAD');
 
-  add: function(path, callback) {
-    childProcess.exec('git add -f ' + path, {}, function(err) {
-      if (err) {
-        throw new Error('git.add: ' + err.message);
-      }
+    const tags = stdout.trim().split(/\n|\r\n/);
+    const versionTags = tags.filter(tag => /^v/.test(tag));
 
-      callback();
-    });
-  },
-  commit: function(name, callback) {
-    childProcess.exec('git commit --message=' + name, {}, function(err) {
-      if (err) {
-        throw new Error('git.commit: ' + err.message);
-      }
-
-      callback();
-    });
-  },
-  tag: function(name, callback) {
-    childProcess.exec('git tag -a --message=' + name + ' ' + name, {}, function(
-      err
-    ) {
-      if (err) {
-        throw new Error('git.tag: ' + err.message);
-      }
-
-      callback();
-    });
-  },
-  tagName: function(callback) {
-    childProcess.exec('git describe --tags', {}, function(err, stdout) {
-      if (err) {
-        throw new Error('git.tagName: ' + err.message);
-      }
-
-      let tags = stdout.trim().split(/\n/);
-      tags = tags.filter(function(info) {
-        info = info.split('-');
-        return info.length == 1;
-      });
-
-      const versionTags = tags.filter(function(info) {
-        return /^v/.test(info[0]);
-      });
-
-      callback(undefined, versionTags[0] || tags[0]);
-    });
+    if (versionTags[0] != null) {
+      return versionTags;
+    }
+    return tags[0];
   }
 };
+
+async function git(...args) {
+  return new Promise((resolve, reject) =>
+    childProcess.execFile('git', args, (err, stdout) => {
+      if (err != null) {
+        return reject(
+          new Error(`"git ${args.join(' ')}" caused error: ${err.message}`)
+        );
+      }
+      resolve(stdout);
+    })
+  );
+}
