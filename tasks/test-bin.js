e46baa1fe23b3a1bee07b085e48700c090218b25
DeeDeeG <DeeDeeG@users.noreply.github.com>
2020-03-31 15:14:17 -0400

    tasks/test-bin.js: Delete duplicate test
    Per comment by @ErisDS on the GitHub PR:
    https://github.com/wycats/handlebars.js/pull/1666#issuecomment-606805248

--- tasks/test-bin.js
+++ tasks/test-bin.js
@@ -161,20 +161,6 @@ const testCases = [
     expectedOutputSpec: './spec/expected/source.map.amd.js'
   },
   {
-    binInputParameters: [
-      '-i',
-      '<div>1</div>',
-      '-a',
-      '-m',
-      '-N',
-      'test',
-      '--map',
-      './spec/artifacts/source.map.amd.txt'
-    ],
-    outputLocation: 'stdout',
-    expectedOutputSpec: './spec/expected/source.map.amd.js'
-  },
-  {
     binInputParameters: ['./spec/artifacts/bom.handlebars', '-b', '-a'],
     outputLocation: 'stdout',
     expectedOutputSpec: './spec/expected/bom.amd.js'
