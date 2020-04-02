f0a2c8075f90ed223417f06aa092f8d68ab42a43
Nils Knappmeier <github@knappi.org>
2020-04-02 08:39:51 +0200

    test: fix integration tests
    - They should fail, if any test fails
    - Use "set -e" instead of "|| exit 1", because it suffices to be specified
      at the top of each file

--- integration-testing/run-integration-tests.sh
+++ integration-testing/run-integration-tests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-cd "$( dirname "$( readlink -f "$0" )" )" || exit 1
+set -e
+
+cd "$( dirname "$( readlink -f "$0" )" )"
 
 for i in */test.sh ; do
   (
     echo "----------------------------------------"
     echo "-- Running integration test: $i"
     echo "----------------------------------------"
-    cd "$( dirname "$i" )" || exit 1
-    ./test.sh || exit 1
+    cd "$( dirname "$i" )"
+    ./test.sh
   )
 done
