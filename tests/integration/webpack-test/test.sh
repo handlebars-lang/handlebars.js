a8261b224535b50fc09ced1642026db0b210a86e
Jakob Linskeseder <jakob@linskeseder.com>
2022-05-15 23:47:33 +0200

    Fix bundler issue with webpack 5
    As explained in issue #1844 and in issue
    https://github.com/webpack/webpack/issues/15007#issuecomment-996615250,
    the way we used the `browser`-field was wrong.
    
    The main reason for using the `browser`-field is the requirement of
    `require('fs')` in the main-entry-file.
    The workaround for this was using `require('handlebars/lib/handlebars')`,
    but now it will also work via `require('handlebars')` for bundlers that
    respect the `browser`-field.
    
    The `"./runtime"`-config was removed, because it didn't have any effect.
    In order to detect regressions, the webpack-integration test was
    extended to test with different webpack versions.
    
    Fixes #1174
    Closes #1844

--- tests/integration/webpack-test/test.sh
+++ tests/integration/webpack-test/test.sh
@@ -2,15 +2,26 @@
 
 set -e
 
+run_tests () {
+  for i in dist/*-test.js ; do
+    echo "----------------------"
+    echo "-- Running $i"
+    echo "----------------------"
+    node "$i"
+    echo "Success"
+  done
+}
+
 # Cleanup: package-lock and "npm ci" is not working with local dependencies
 rm dist package-lock.json -rf
 npm install --legacy-peer-deps
-npm run build
 
-for i in dist/*-test.js ; do
-  echo "----------------------"
-  echo "-- Running $i"
-  echo "----------------------"
-  node "$i"
-  echo "Success"
-done
+# Test with webpack 4
+npm install --legacy-peer-deps --no-save webpack@^4 webpack-cli@^3
+npm run build
+run_tests
+
+# Test with webpack 5
+npm install --legacy-peer-deps --no-save webpack@^5 webpack-cli@^4
+npm run build
+run_tests
