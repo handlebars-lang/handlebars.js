e3a54485db7730b74656450373b5f774364ffe34
Jakob Linskeseder <jakob@linskeseder.com>
2022-05-15 23:47:33 +0200

    Fix bundler issue with webpack 5
    As explained in issue #1844 and in issue
    https://github.com/webpack/webpack/issues/15007#issuecomment-996615250,
    the way we used the `browser`-field was wrong.
    
    The main reason for using the `browser`-field is the requirement of
    `require('fs')` in the main-entry-file.
    The workaround for this was using `require('handlebars/lib/handlebars')`,
    but now it will also work via `require('handlebars')` for bundlers that
    respect the `browser`-field.
    
    The `"./runtime"`-config was removed, because it didn't have any effect.
    In order to detect regressions, the webpack-integration test was
    extended to test with different webpack versions.
    
    Fixes #1174
    Closes #1844

---
+++ tests/integration/webpack-test/src/handlebars-runtime-test.js
@@ -0,0 +1,43 @@
+import * as Handlebars from 'handlebars/runtime';
+import { assertEquals } from './lib/assert';
+
+const template = Handlebars.template({
+  compiler: [8, '>= 4.3.0'],
+  main: function(container, depth0, helpers, partials, data) {
+    var helper,
+      lookupProperty =
+        container.lookupProperty ||
+        function(parent, propertyName) {
+          if (Object.prototype.hasOwnProperty.call(parent, propertyName)) {
+            return parent[propertyName];
+          }
+          return undefined;
+        };
+
+    return (
+      'Author: ' +
+      container.escapeExpression(
+        ((helper =
+          (helper =
+            lookupProperty(helpers, 'author') ||
+            (depth0 != null ? lookupProperty(depth0, 'author') : depth0)) !=
+          null
+            ? helper
+            : container.hooks.helperMissing),
+        typeof helper === 'function'
+          ? helper.call(depth0 != null ? depth0 : container.nullContext || {}, {
+              name: 'author',
+              hash: {},
+              data: data,
+              loc: {
+                start: { line: 1, column: 8 },
+                end: { line: 1, column: 18 }
+              }
+            })
+          : helper)
+      )
+    );
+  },
+  useData: true
+});
+assertEquals(template({ author: 'Yehuda' }), 'Author: Yehuda');
