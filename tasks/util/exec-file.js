903ca504b5af1df18fbd90ed00aa3412c5b99101
Jakob Linskeseder <jakob.linskeseder@karriere.at>
2022-10-14 13:06:51 +0200

    Remove unused parser tests
    Parser tests are not used anymore, since the parser is
    now living in a separate package.

--- tasks/util/exec-file.js
+++ tasks/util/exec-file.js
@@ -1,10 +1,7 @@
 const childProcess = require('child_process');
-const fs = require('fs');
-const path = require('path');
 
 module.exports = {
-  execNodeJsScriptWithInheritedOutput,
-  execFileWithInheritedOutput
+  execNodeJsScriptWithInheritedOutput
 };
 
 async function execNodeJsScriptWithInheritedOutput(command, args) {
@@ -17,35 +14,4 @@ async function execNodeJsScriptWithInheritedOutput(command, args) {
       resolve();
     });
   });
-}
-
-async function execFileWithInheritedOutput(command, args) {
-  return new Promise((resolve, reject) => {
-    const resolvedCommand = preferLocalDependencies(command);
-    const child = childProcess.spawn(resolvedCommand, args, {
-      stdio: 'inherit'
-    });
-    child.on('exit', code => {
-      if (code !== 0) {
-        reject(new Error(`Child process failed with exit-code ${code}`));
-      }
-      resolve();
-    });
-  });
-}
-
-function preferLocalDependencies(command) {
-  const localCandidate = resolveLocalCandidate(command);
-
-  if (fs.existsSync(localCandidate)) {
-    return localCandidate;
-  }
-  return command;
-}
-
-function resolveLocalCandidate(command) {
-  if (process.platform === 'win32') {
-    return path.join('node_modules', '.bin', command + '.cmd');
-  }
-  return path.join('node_modules', '.bin', command);
 }
